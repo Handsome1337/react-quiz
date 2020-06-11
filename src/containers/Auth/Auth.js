961d06a207832fd1fc7ca8ac4fb695b1c568011c
Ilya Volkov <volkov-ilyha@ya.ru>
2020-06-11 15:20:43 +0300

    Добавляет валидацию формы

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import is from 'is_js';
 export default class Auth extends Component {
 
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: '',
@@ -79,7 +80,14 @@ export default class Auth extends Component {
 
     formControls[controlName] = control;
 
+    let isFormValid = true;
+
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid;
+    });
+
     this.setState({
+      isFormValid,
       formControls
     });
   };
@@ -116,6 +124,7 @@ export default class Auth extends Component {
             <Button
               type="success"
               onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
             >
               Войти
             </Button>
@@ -123,6 +132,7 @@ export default class Auth extends Component {
             <Button
               type="primary"
               onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
             >
               Зарегистрироваться
             </Button>
